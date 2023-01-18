e68f5561490565fc8644f4f8044c125d3844e5b3
sureshsahu <suresh.sahu@trangile.com>
2023-01-18 21:53:14 +0530

    code_deployment

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,34 +1,51 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib"
-import { CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda"
+import { Alias, CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda"
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Construct } from "constructs"
 import { HttpApi } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { LambdaDeploymentConfig, LambdaDeploymentGroup } from "aws-cdk-lib/aws-codedeploy";
 
 interface ServiceStackProps extends StackProps {
     stageName: string;
-}
+  }
+  
 export class ServiceStack extends Stack {
+    
     public readonly serviceCode: CfnParametersCode;
-    // public readonly api: HttpApi;
     public readonly serviceEndpointOutput: CfnOutput;
     constructor(scope: Construct, id: string, props: ServiceStackProps) {
         super(scope, id, props)
         this.serviceCode = Code.fromCfnParameters();
-        const lambda = new Function(this, "serviceLambda", {
+        const lambda=new Function(this, "serviceLambda", {
             runtime: Runtime.NODEJS_16_X,
             handler: "src/lambda.handler",
             code: this.serviceCode,
             functionName: `ServiceLambda${props.stageName}`,
+            description: `Generated on ${new Date().toISOString()}`,
         })
-        const httpApi =  new HttpApi(this, "ServiceApi", {
-            defaultIntegration: new HttpLambdaIntegration("LambdaIntegration", lambda),
-            apiName: `MyService${props.stageName}`
+
+        const alias = new Alias(this, "ServiceLambdaAlias", {
+            version: lambda.currentVersion,
+            aliasName: `ServiceLambdaAlias${props.stageName}`,
+          });
+          
+       const httpApi= new HttpApi(this,"ServiceApi",{
+            defaultIntegration:new HttpLambdaIntegration("LambdaIntegration",alias),
+            apiName: `MyService${props.stageName}`,
         })
+        
+    if (props.stageName === "Prod") {
+        new LambdaDeploymentGroup(this, "DeploymentGroup", {
+          alias: alias,
+          deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+        });
+      }
+
         this.serviceEndpointOutput = new CfnOutput(this, "ApiEndpointOutput", {
             exportName: `ServiceEndpoint${props.stageName}`,
             value: httpApi.apiEndpoint,
             description: "API Endpoint",
           });
     }
-
+    
 }
