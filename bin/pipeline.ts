0dfa63ac94a859cf5e9181f43d8022ee9780e26a
sureshsahu <suresh.sahu@trangile.com>
2023-01-18 09:25:13 +0530

    testing

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -20,7 +20,7 @@ const serviceStackProd = new ServiceStack(app, "ServiceStackProd", {
 //   emailAddress: "sureshsahu1453@gmail.com",
 // });
 // pipelineStack.addServiceStage(serviceStackProd,"service")
-pipelineStack.addServiceStage(serviceStackProd, "ServiceStackProd");
 pipelineStack.addServiceStage(serviceStackTest, "Test");
+pipelineStack.addServiceStage(serviceStackProd, "ServiceStackProd");
 
 
