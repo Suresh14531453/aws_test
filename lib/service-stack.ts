3e0c94a2f00e2cae2906a7b0335f3b68dcbde9aa
sureshsahu <suresh.sahu@trangile.com>
2023-01-18 14:29:50 +0530

    test_update_files

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib"
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib"
 import { CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda"
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Construct } from "constructs"
@@ -8,9 +8,10 @@ interface ServiceStackProps extends StackProps {
     stageName: string;
 }
 export class ServiceStack extends Stack {
-
     public readonly serviceCode: CfnParametersCode;
-    constructor(scope: Construct, id: string, props:ServiceStackProps) {
+    // public readonly api: HttpApi;
+    public readonly serviceEndpointOutput: CfnOutput;
+    constructor(scope: Construct, id: string, props: ServiceStackProps) {
         super(scope, id, props)
         this.serviceCode = Code.fromCfnParameters();
         const lambda = new Function(this, "serviceLambda", {
@@ -19,10 +20,15 @@ export class ServiceStack extends Stack {
             code: this.serviceCode,
             functionName: `ServiceLambda${props.stageName}`,
         })
-        new HttpApi(this, "ServiceApi", {
+        const httpApi =  new HttpApi(this, "ServiceApi", {
             defaultIntegration: new HttpLambdaIntegration("LambdaIntegration", lambda),
             apiName: `MyService${props.stageName}`
         })
+        this.serviceEndpointOutput = new CfnOutput(this, "ApiEndpointOutput", {
+            exportName: `ServiceEndpoint${props.stageName}`,
+            value: httpApi.apiEndpoint,
+            description: "API Endpoint",
+          });
     }
 
 }
